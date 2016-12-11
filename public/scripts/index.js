50d09ec269cfc979161288db1702ed0f6a69b586
hkdnet <hkdnet@users.noreply.github.com>
2016-12-12 07:34:38 +0900

    Show template buttons

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,5 +1,5 @@
-(function() {
-  var read = function() {
+(function () {
+  var read = function () {
     var raw = localStorage.getItem('data');
     if (!raw) {
       return;
@@ -7,10 +7,11 @@
     var data = JSON.parse(raw);
     return data;
   };
-  var write = function(data) {
+  var write = function (data) {
     localStorage.setItem('data', JSON.stringify(data));
   };
-  var data = read() ||{
+
+  var data = read() || {
     messages: [],
     url: "/message",
     type: "text",
@@ -20,8 +21,8 @@
     el: '#app',
     data: data,
     computed: {
-      body: function() {
-        var message = { type: this.type, text: this.text};
+      body: function () {
+        var message = {type: this.type, text: this.text};
         var data = {
           events: [{
             replyToken: "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
@@ -36,9 +37,51 @@
         };
         return JSON.stringify(data, null, "  ");
       }
+    },
+    methods: {
+      buttonAction: function (e) {
+        var target = e.target;
+        if (!target) return false;
+        var type = target.getAttribute('data-type');
+        switch (type) {
+          case "postback":
+            var data = {
+              events: [{
+                "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
+                "type": "postback",
+                "timestamp": 1462629479859,
+                "source": {
+                  "type": "user",
+                  "userId": "U206d25c2ea6bd87c17655609a1c37cb8"
+                },
+                "postback": {
+                  "data": target.getAttribute('data-data')
+                }
+              }]
+            };
+            send(JSON.stringify(data));
+            break;
+          default:
+        }
+      },
+      sendMessage: function (e) {
+        send(this.body);
+        e.preventDefault();
+      }
     }
   });
-  window.addEventListener('beforeunload', function() {
+  var send = function (data) {
+    var option = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      cors: true,
+      body: data
+    };
+    fetch(app.url, option);
+  };
+  window.addEventListener('beforeunload', function () {
     var data = {
       messages: [],
       url: app.url,
@@ -49,27 +92,11 @@
   });
   var socket = io.connect();
 
-  socket.on('message', function(reply) {
-    console.log(reply)
-    if(!reply || !reply.messages) {
+  socket.on('message', function (reply) {
+    if (!reply || !reply.messages) {
       console.error('Format error');
+      return;
     }
     reply.messages.forEach(e => app.messages.push(e));
   });
-
-  document
-    .getElementById('submit')
-    .addEventListener('click', function(e) {
-      var data = document.querySelector('#result');
-      var option = {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        cors: true,
-        body: data.textContent
-     };
-     fetch(app.url, option)
-     e.preventDefault();
-    });
 })();
